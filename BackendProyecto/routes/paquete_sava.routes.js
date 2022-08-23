9d4276d641f67b79f8aef519b8b7be327be41060
Alfreed0 <lurochav@espol.edu.ec>
2022-08-22 20:05:17 -0500

    Implementacion de endpoints para historial

--- BackendProyecto/routes/paquete_sava.routes.js
+++ BackendProyecto/routes/paquete_sava.routes.js
@@ -3,8 +3,11 @@ var router = express.Router();
 var PaqueteSavaController=require('../controllers/paquete_sava.controller.js')
 
 router.get('/', PaqueteSavaController.list);
-router.get('/all',PaqueteSavaController.listAll)
-router.put("/edit",PaqueteSavaController.Modify)
+router.get('/all',PaqueteSavaController.listAll);
+router.put("/edit",PaqueteSavaController.Modify);
+router.get('/history',PaqueteSavaController.filterDelivered);
+router.get('/orderHistory',PaqueteSavaController.orderDate);
+router.get('/betweenHistory',PaqueteSavaController.filterDate);
 
 
 
