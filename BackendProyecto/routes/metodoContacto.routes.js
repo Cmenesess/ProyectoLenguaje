fabe05e2f5d401505d55f945df9bf006768e7199
Carlos Meneses <cmeneses@espol.edu.ec>
2022-09-05 20:39:24 -0500

    modifying the database

--- BackendProyecto/routes/metodoContacto.routes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var metodoController= require('../controllers/MetodoContacto.controller');
-
-router.post('/crear',metodoController.CreateContacto);
-
-
-module.exports = router;
